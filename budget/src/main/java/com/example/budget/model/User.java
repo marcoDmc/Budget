9c256e55485025c7a5937e5c03e5eb34a3529272
marcoDmc <marcodamascenodev@gmail.com>
2025-05-09 17:53:04 -0300

    importing properties for create an user entity

--- budget/src/main/java/com/example/budget/model/User.java
+++ budget/src/main/java/com/example/budget/model/User.java
@@ -9,7 +9,19 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
