63f3e7340d9fca707c4a3374d5dca049a1bb6d43
marcoDmc <marcodamascenodev@gmail.com>
2025-05-09 17:50:00 -0300

    importing annotations for handle requests

--- budget/src/main/java/com/example/budget/controllers/user/UserController.java
+++ budget/src/main/java/com/example/budget/controllers/user/UserController.java
@@ -3,10 +3,7 @@ package com.example.budget.controllers.user;
 import com.example.budget.dtos.DeleteUserDTO;
 import com.example.budget.dtos.ForgotPasswordDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.budget.model.User;
 import com.example.budget.services.UserServices;
