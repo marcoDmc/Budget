9711f9dbe6e42c3f7a1804eda7cb315f72c1fea4
marcoDmc <marcodamascenodev@gmail.com>
2025-05-24 19:07:01 -0300

    chore: New repositories were created, namely the account entity, the category entity and history

---
+++ budget/src/main/java/com/example/budget/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.example.budget.repository;
+
+import com.example.budget.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
